4158c353fb788dfa7eb3db7b2884a9456920de4f
Clayton Siby <claytonsiby@gmail.com>
2023-10-10 13:04:14 +0200

    Update: package.json engines to run latest node and npm

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,10 @@
-const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common.js");
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common.js')
 
 module.exports = (environmentVariables) => {
-  const { env } = environmentVariables;
-  const environmentConfig = require(`./webpack.${env}.js`);
-  const config = merge(commonConfig, environmentConfig);
+  const { env } = environmentVariables
+  const environmentConfig = require(`./webpack.${env}.js`)
+  const config = merge(commonConfig, environmentConfig)
 
-  return config;
+  return config
 }
